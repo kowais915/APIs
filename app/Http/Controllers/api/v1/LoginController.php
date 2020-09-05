c7f060c807bc0d87d1f048bc70f19bc2a32b6a1f
Owais Khan <kowais915@gmail.com>
2020-09-05 17:50:29 +0500

    Installed passport and created a passport function

---
+++ app/Http/Controllers/api/v1/LoginController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\api\v1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\support\facades\Auth;
+class LoginController extends Controller
+{
+    public function login( Request $request) {
+
+        $login = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+        if(!Auth::attempt($login)){
+            return response(['message' => 'Invalid credentials!'])
+        }
+    }
+}
